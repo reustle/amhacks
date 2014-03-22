1c0441c878d678afed515b539073881df95421dd
Shane Reustle <me@shanereustle.com>
2014-03-22 00:27:14 -0400

    Rewrote everything

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -1,16 +1,5 @@
-// Mailchimp submit
-$('#apply_form').ajaxChimp({
-	callback : function(res){
-		if(res.result == 'success'){
-			alert('Almost finished... We need to confirm your email address. To complete the application process, please click the link in the email we just sent you. See you soon!')
-			window.location = '/';
-		}
-	},
-	url : 'http://reustle.us7.list-manage2.com/subscribe/post?u=7529e30edd60b5d1f5ba46e68&id=ac896a0887'
-});
-
 // Submit button press
-$('#btn_apply').click(function(){
+$('.panel-apply button').click(function(){
 	
 	// Read form
 	var form_data = {
@@ -34,8 +23,8 @@ $('#btn_apply').click(function(){
 		alert('Please enter your twitter handle');
 		return false;
 	}
-	if(form_data.twitter.length < 2){
-		alert('Please enter your twitter handle');
+	if(form_data.github.length < 2){
+		alert('Please enter your github username');
 		return false;
 	}
 	if(form_data.hack.length < 5){
@@ -43,7 +32,26 @@ $('#btn_apply').click(function(){
 		return false;
 	}
 	
-	$('#apply_form').submit();
+	// Let the user know we are loading
+	$('.panel-apply button').html('Sending...').addClass('disabled');
+	$(this).blur();
+	
+	// Submit the form
+	$('.panel-apply form').submit();
 	
 });
+
+// Mailchimp Submit
+$('.panel-apply form').ajaxChimp({
+	callback : function(res){
+		
+		$('.panel-apply form').slideUp();
+		$('.application-complete').slideDown();
+		
+	},
+	url : 'http://reustle.us7.list-manage2.com/subscribe/post?u=7529e30edd60b5d1f5ba46e68&id=ac896a0887'
+});
+
+// Hide this for now
+$('.application-complete').hide();
 
